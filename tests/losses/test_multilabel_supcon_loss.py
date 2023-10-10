41113b9d8344a29595355a76b43751ded9051116
Qibin Liang <46101145+PhyseChan@users.noreply.github.com>
2023-10-10 19:47:12 +0800

    [update] reformat testcase

--- tests/losses/test_multilabel_supcon_loss.py
+++ tests/losses/test_multilabel_supcon_loss.py
@@ -10,66 +10,38 @@ from pytorch_metric_learning.losses import (
 )
 
 class TestMultiSupConLoss(unittest.TestCase):
-    def test_multi_supcon_loss(self):
-        n_cls = 6
-        n_samples = 6
-        n_dim = 5
-        loss_func = MultiSupConLoss(num_classes=n_cls)
-        xbm_loss_func = CrossBatchMemory4MultiLabel(loss_func, n_dim, memory_size=16)
-
-        # test float32 and float64
-        for dtype in [torch.float32, torch.float64]:
-            embeddings = torch.randn(n_samples, n_dim, dtype=dtype)
-            labels = [random.sample(range(n_cls), np.random.randint(1, 4)) for i in range(n_samples)]
-            loss = loss_func(embeddings, labels)
-            self.assertTrue(loss >= 0)
-
-        # test cuda and cpu
-        for device in [torch.device("cpu"),torch.device("cuda")]:
-            embeddings = torch.randn(n_samples, n_dim, device=device)
-            labels = [random.sample(range(n_cls), np.random.randint(1, 4)) for i in range(n_samples)]
-            loss = loss_func(embeddings, labels)
-            self.assertTrue(loss >= 0)
-
-        # test xbm
-        batchs = 4
-        for b in range(batchs):
-            embeddings = torch.randn(n_samples, n_dim, dtype=torch.float32)
-            labels = [random.sample(range(n_cls), np.random.randint(1, 4)) for i in range(n_samples)]
-            loss = xbm_loss_func(embeddings, labels)
-        self.assertTrue(loss >= 0)
-
-        # test scatter labels
-        for device in [torch.device("cpu"),torch.device("cuda")]:
-            embeddings = torch.randn(n_samples, n_dim, device=device)
-            labels = [random.sample(range(n_cls), np.random.randint(1, 4)) for i in range(n_samples)]
-            labels = torch.stack([
-                torch.nn.functional.one_hot(torch.tensor(label), n_cls).sum(dim=0).float()
-                for label in labels
-            ], dim=0)
-            loss = loss_func(embeddings, labels)
-            self.assertTrue(loss >= 0)
-
-        # test val
-        embeddings = torch.tensor([[0.1, 0.3, 0.1],
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.n_cls = 3
+        self.n_samples = 4
+        self.n_dim = 3
+        self.n_batchs = 10
+        self.xbm_max_size = 1024
+        self.loss_func = MultiSupConLoss(
+                        num_classes=self.n_cls, 
+                        threshold=0.3)
+        self.xbm_loss_func = CrossBatchMemory4MultiLabel(
+                        self.loss_func, 
+                        self.n_dim, 
+                        memory_size=self.xbm_max_size)
+        # test cases
+        self.embeddings = torch.tensor([[0.1, 0.3, 0.1],
                                 [0.23, -0.2, -0.1],
                                 [0.1, -0.16, 0.1],
                                 [0.13, -0.13, 0.2]])
-        labels = torch.tensor([[1,0,1], [1,0,0], [0,1,1], [0,1,0]])
-        affine_net = torch.nn.Linear(3, 3, bias=False)
-        affine_net.weight.data = torch.ones_like(affine_net.weight.data)
-        embeddings = affine_net(embeddings)
-        loss = loss_func(embeddings, labels)
-        self.assertTrue(loss>0)
+        self.labels = torch.tensor([[1,0,1], [1,0,0], [0,1,1], [0,1,0]])
+        self.test_multisupcon_val_gt = 0.6247
+        # xbm test cases
+        self.test_xbm_multisupcon_val_gt = 2.3841
 
+
+    def test_multisupcon_val(self):
+        loss = self.loss_func(self.embeddings, self.labels)
+        print(loss)
+        self.assertTrue(np.isclose(loss.item(), self.test_multisupcon_val_gt, atol=1e-4))
+
+    def test_xbm_multisupcon_val(self):
         # test xbm with scatter labels
-        batchs = 4
-        for b in range(batchs):            
-            embeddings = torch.randn(n_samples, n_dim)
-            labels = [random.sample(range(n_cls), np.random.randint(1, 4)) for i in range(n_samples)]
-            labels = torch.stack([
-                torch.nn.functional.one_hot(torch.tensor(label), n_cls).sum(dim=0).float()
-                for label in labels
-            ], dim=0)
-            loss = loss_func(embeddings, labels)
-        self.assertTrue(loss > 0)
+        for b in range(self.n_batchs):            
+            loss = self.xbm_loss_func(self.embeddings, self.labels)
+        self.assertTrue(np.isclose(loss.item(), self.test_xbm_multisupcon_val_gt, atol=1e-4))
